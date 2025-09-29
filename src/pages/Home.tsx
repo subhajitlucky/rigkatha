7110446f2f7e14e8578272e26ec7e1a6f2ae7ccf
deepesh-sr <220714100078@cutm.ac.in>
2025-09-29 13:00:38 +0530

    initial setup and home page added

---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+const Home = () => {
+  return (
+    <div 
+      className='h-screen flex justify-center items-center bg-cover bg-center bg-no-repeat'
+      style={{ backgroundImage: 'url("/bg.jpg")' }}
+    >
+      <h1 className='text-3xl sm:text-5xl md:text-9xl lg:text-[12vw] text-amber-900 drop-shadow-2xl'>
+        ऋग्कथा
+      </h1>
+    </div>
+  )
+}
+
+export default Home
