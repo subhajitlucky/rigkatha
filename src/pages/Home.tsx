3857a0728dff1783bc897cfd8e19087aea359ace
deepesh-sr <220714100078@cutm.ac.in>
2025-09-29 13:08:40 +0530

    bg image updated

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,8 @@
 const Home = () => {
   return (
     <div 
-      className='h-screen flex justify-center items-center bg-cover bg-center bg-no-repeat'
-      style={{ backgroundImage: 'url("/bg.jpg")' }}
+      className='h-screen flex justify-center items-center bg-cover bg-center  bg-no-repeat'
+      style={{ backgroundImage: 'url("/bg2.jpg")'}}
     >
       <h1 className='text-3xl sm:text-5xl md:text-9xl lg:text-[12vw] text-amber-900 drop-shadow-2xl'>
         ऋग्कथा
